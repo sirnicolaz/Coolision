4886e81bce3d71fee0620684968e3b06e8584280
Bruno Verneau <devnull@localhost>
2009-08-21 18:43:05 +0200

    View.php fixed (weird stuff removed)

--- app/code/local/Coolision/Catalog/Block/Layer/View.php
+++ app/code/local/Coolision/Catalog/Block/Layer/View.php
@@ -5,7 +5,6 @@ class Coolision_Catalog_Block_Layer_View extends Mage_Catalog_Block_Layer_View
     /* Funzione per recuperare la lista dei nomi dei filtri disponibili */
     public function getFiltersName()
     {
-      cjdwnsjwn
       /* Recupera i filtri disponibili */
       $filters = $this->getFilters();
 
