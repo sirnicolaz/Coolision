aca4861c280a806ea0583c5e42fc8aef083bbbd0
Andrea Graziano <andrea.graz@gmail.com>
2009-08-21 10:55:23 +0200

    fixed some stuff (layer navigation)

--- app/code/local/Coolision/Catalog/Block/Layer/Filter/Abstract.php
+++ app/code/local/Coolision/Catalog/Block/Layer/Filter/Abstract.php
@@ -5,6 +5,7 @@ abstract class Coolision_Catalog_Block_Layer_Filter_Abstract extends Mage_Catalo
   /* Funzione per prendere l'attribute code del filtro */   
   public function getAttributeCode()
   {
+      jewhfowefew
       return $this->_filter->getRequestVar();
   }
 }
