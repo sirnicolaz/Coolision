bec3aa8c081c9f23034329389801c6934550d139
Nicola Miotto <sirnicolaz@gmail.com>
2009-09-01 19:09:49 +0200

    Blasfemies over blasfemies in shaping horizontal-menu on client service page.

--- skin/frontend/coolision/default/js/horizontal-menu.js
+++ skin/frontend/coolision/default/js/horizontal-menu.js
@@ -1,15 +1,15 @@
-function changeTab(tabName){
+function changeTab(tabName, foreActive, foreInactive, backActive, backInactive){
 						for(var j=0; j<document.getElementById('menuContainer').getElementsByTagName('li').length; j++){
 							var menuTab = document.getElementById('menuContainer').getElementsByTagName('li')[j];
 							if(menuTab.id==tabName){
 								document.getElementById('menuContainer').getElementsByTagName('li')[j].class='active';
-								document.getElementById('menuContainer').getElementsByTagName('li')[j].getElementsByTagName('a')[0].style.color= '#262626';
-								document.getElementById('menuContainer').getElementsByTagName('li')[j].style.background = '#FCCA21';
+								document.getElementById('menuContainer').getElementsByTagName('li')[j].getElementsByTagName('a')[0].style.color= foreActive;
+								document.getElementById('menuContainer').getElementsByTagName('li')[j].style.background = backActive;
 							}
 							else{ 
 								document.getElementById('menuContainer').getElementsByTagName('li')[j].class='inactive';
-								document.getElementById('menuContainer').getElementsByTagName('li')[j].getElementsByTagName('a')[0].style.color= 'white';
-								document.getElementById('menuContainer').getElementsByTagName('li')[j].style.background = '#262626';
+								document.getElementById('menuContainer').getElementsByTagName('li')[j].getElementsByTagName('a')[0].style.color= foreInactive;
+								document.getElementById('menuContainer').getElementsByTagName('li')[j].style.background = backInactive;
 	
 							}
 						}
