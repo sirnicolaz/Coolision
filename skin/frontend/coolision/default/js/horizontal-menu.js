de76391e6e44c981dad73b90630a1c77f2a125db
Bruno Verneau <devnull@localhost>
2009-09-03 18:42:18 +0200

    solved problem javascript TAB in IE

--- skin/frontend/coolision/default/js/horizontal-menu.js
+++ skin/frontend/coolision/default/js/horizontal-menu.js
@@ -2,12 +2,12 @@
 						for(var j=0; j<document.getElementById('menuContainer').getElementsByTagName('li').length; j++){
 							var menuTab = document.getElementById('menuContainer').getElementsByTagName('li')[j];
 							if(menuTab.id==tabName){
-								document.getElementById('menuContainer').getElementsByTagName('li')[j].class='active';
+								document.getElementById('menuContainer').getElementsByTagName('li')[j].className='active';
 								document.getElementById('menuContainer').getElementsByTagName('li')[j].getElementsByTagName('a')[0].style.color= foreActive;
 								document.getElementById('menuContainer').getElementsByTagName('li')[j].style.background = backActive;
 							}
 							else{ 
-								document.getElementById('menuContainer').getElementsByTagName('li')[j].class='inactive';
+								document.getElementById('menuContainer').getElementsByTagName('li')[j].className='inactive';
 								document.getElementById('menuContainer').getElementsByTagName('li')[j].getElementsByTagName('a')[0].style.color= foreInactive;
 								document.getElementById('menuContainer').getElementsByTagName('li')[j].style.background = backInactive;
 	
